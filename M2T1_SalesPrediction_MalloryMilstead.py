876232f7e586bfddcaccac37c85d0367beb57dd7
milsteam4144 <milsteam4144@student.faytechcc.edu>
2017-06-11 18:09:26 -0400

    Add files via upload
    First upload of a python code to github.

---
+++ M2T1_SalesPrediction_MalloryMilstead.py
@@ -0,0 +1,21 @@
+#Write a program to predict profit
+#6/11/2017
+#CTI-110 M2T1 - Sales Prediction
+#Mallory Milstead
+
+#Get the projected total sales as a decimal number.
+total_sales= float(input('Enter the projected sales: '))
+
+#Calculate the profit as 23 percent of total sales.
+profit=total_sales*0.23
+
+#Display profit.
+print("The profit is $", format(profit, ',.2f'))
+
+
+
+
+
+
+
+
