9c403eea527c18528ef1a1105f265ce754201208
milsteam4144 <milsteam4144@student.faytechcc.edu>
2017-06-11 19:54:30 -0400

    Add files via upload
    Simple program to calculate distance traveled. (Beginner)

---
+++ M2HW1_DistanceTraveled_Milstead.py
@@ -0,0 +1,32 @@
+# Calculating Distance Traveled
+# 6/11/2017
+# CTI-110 M2HW1 - Distance Traveled 
+# Mallory Milstead
+
+#State that car is traveling at 70mph.
+speed=70
+
+#State that in this instance, the time traveled will be 6 hours.
+time=6
+
+#Calculate distance.
+distance=speed*time
+
+#Display the distance traveld in six hours.
+print("In six hours, the car will travel" ,distance, "miles.")
+
+#Change time traveled.
+time=10
+
+#Display the distance traveled in ten hours.
+print("In ten hours, the car will travel" ,(speed*time), "miles.")
+
+#Change time traveled.
+time=15
+
+#Display the distance traveled in fifteen hours.
+print("In fifteen hours, the car will travel" ,(speed*time), "miles.")
+
+
+
+
