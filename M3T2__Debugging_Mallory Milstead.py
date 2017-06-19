3551e2a6a93ac6090d8f6d3aeecd01a0582cecdb
milsteam4144 <milsteam4144@student.faytechcc.edu>
2017-06-18 23:30:43 -0400

    Add files via upload
    Adding indentation to a program that was once lacking it. (Understanding indentation in if-else statements)

---
+++ M3T2__Debugging_Mallory Milstead.py
@@ -0,0 +1,18 @@
+# Use proper conventions of alignment and indentation.
+# 6/18/17
+# CTI-110 M3T2 - Debugging
+# Mallory Milstead
+#
+if score >= A_score:
+    print('Your grade is A.')
+else:
+    if score >= B_score:
+        print('Your grade is B.')
+    else:
+        if score >= C_score:
+            print('Your grade is C.')
+        else:
+            if score >= D_score:
+                print('Your grade is D.')
+            else:
+                print('Your grade is F.')
